3d4544b401ba109fe335fd6179d9ff52102b7e92
Micah <micahmccallum@outlook.com>
2021-02-13 20:04:36 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,20 @@ class SongsController < ApplicationController
     erb :'/songs/index'
   end
 
+  get '/songs/new' do
+    @genres = Genre.all
+    erb :'/songs/new'
+  end
+
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
   end
 
+  
+
+  post '/songs' do
+    binding.pry
+    
+  end
 end
