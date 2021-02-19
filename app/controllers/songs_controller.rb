a5d729d8f06af50f7f4440c35495c5cd72fb1b54
Micah <micahmccallum@outlook.com>
2021-02-19 13:58:20 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,9 @@
+require 'rack-flash'
+
 class SongsController < ApplicationController
+  enable :sessions
+  use Rack::Flash
+  set :method_override, true
 
   get '/songs' do
     @songs = Song.all
@@ -7,23 +12,42 @@ class SongsController < ApplicationController
 
   get '/songs/new' do
     @genres = Genre.all
+
     erb :'/songs/new'
   end
 
   get '/songs/:slug' do
     @song = Song.find_by_slug(params[:slug])
     erb :'/songs/show'
-  end
+  end  
 
-  
+  get '/songs/:slug/edit' do
+    @song = Song.find_by_slug(params[:slug])
+    @genres = Genre.all
+    erb :'/songs/edit'
+  end
 
   post '/songs' do
     Song.exists?(name: params[:song][:name]) ? (erb :'/songs/show') : @song = Song.create(params[:song])
     Artist.exists?(name: params[:artist][:name]) ? @song.artist = Artist.find_by(name: params[:artist][:name]) : @song.artist = Artist.create(params[:artist])
-    params[:song_genre][:genre_ids].each do |genre|
+    params[:genres].each do |genre|
       @song.genres << Genre.find(genre)
     end
     @song.save
+    flash[:message] = "Successfully created song."
     redirect "/songs/#{@song.slug}"
-  end
+  end  
+
+  put '/songs/:slug' do
+    @song = Song.find_by_slug(params[:slug])
+    Artist.exists?(name: params[:artist][:name]) ? @song.artist = Artist.find_by(name: params[:artist][:name]) : @song.artist = Artist.create(params[:artist])
+    @song.genres = []
+    params[:genres].each do |genre|
+      @song.genres << Genre.find(genre)
+    end
+    @song.save
+    flash[:message] = "Successfully updated song."
+    redirect "/songs/#{@song.slug}"
+  end 
+
 end
